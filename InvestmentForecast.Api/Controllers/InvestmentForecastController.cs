8a53eef7c874e171201f850042f2f57360c709c9
Bill Hire <c1983c@hotmail.com>
2018-05-27 22:24:40 +0000

    Added request DTO

---
+++ InvestmentForecast.Api/Controllers/InvestmentForecastController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InvestmentForecast.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvestmentForecast.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class InvestmentForecastController : Controller
+    {
+        [HttpPost]
+        public IActionResult Calculate(ForecastRequestDTO request)
+        {
+            return Ok(20);
+        }
+    }
+}
---
+++ InvestmentForecast.Api/Models/ForecastRequestDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvestmentForecast.Api.Models
+{
+    public class ForecastRequestDTO : IValidatableObject
+    {
+        [Range(0, 100000000.00)]
+        public decimal LumpSumInvestment { get; set; }
+
+        [Range(0, 100000000.00)]
+        public decimal MonthlyInvestment { get; set; }
+
+        [Required]
+        [Range(1, 100)]
+        public int TimeScale { get; set; }
+
+        public string RiskLevel { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            switch (RiskLevel.ToLower())
+            {
+                case "low":
+                case "medium":
+                case "high":
+                   break;
+                default:
+                    yield return new ValidationResult($"Invalid risk level use low, medium, high", new[] { nameof(RiskLevel) });
+                    break;
+            }
+        }
+    }
+}
